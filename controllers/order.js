a882851d5b50c00cd475094b7bba0eb99b910a67
SnoWSTorM27 <snowstorm27@ya.ru>
2022-06-06 23:10:21 +0700

    add angular client

--- controllers/order.js
+++ controllers/order.js
@@ -1,3 +1,4 @@
+const { max } = require('moment');
 const db = require('../db')
 const errorHandler = require('../utils/errorHandler')
 
@@ -13,17 +14,15 @@ module.exports.getAll = async function(req, res) {
 }
 
 module.exports.create = async function(req, res) {
-  
   try { 
-    const lastOrder = await db?.query(`SELECT MAX("id") FROM Orders`).rows[0];
-    console.log('lastOrder:', lastOrder);
+    const lastOrder = await (await db.query(`SELECT MAX("id") FROM Orders`)).rows[0].max;
     const maxOrder = lastOrder ? lastOrder : 0;
     const user_id = req.user.id;
     const {name, quantity} = req.body
-    const cost = await db.query(`SELECT cost FROM Positions where name = $1`, [name]);
+    const cost = (await db.query(`SELECT cost FROM Positions where name = $1`, [name])).rows[0].cost;
     const newOrder = await db.query(`INSERT INTO Orders (id, user_id) values ($1, $2) RETURNING *`, [maxOrder + 1, user_id]);
-    const newSales = await db.query(`INSERT INTO Sales (name_sale, quantity, cost_sale) values ($1, $2, $3) RETURNING *`, [name, quantity, cost]);
-    res.status(201).json(newOrder.rows[0], newSales.rows[0])
+    const newSales = await db.query(`INSERT INTO Sales (order_id, name_sale, quantity, cost_sale) values ($1, $2, $3, $4) RETURNING *`, [maxOrder + 1, name, quantity, cost]);
+    res.status(201).json(newOrder.rows[0])
   } catch (e) {
     errorHandler(res, e)
   }
